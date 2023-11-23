a784e09166d55b753e3f25727a6844a294af57dd
aeokseung <oksh3012@naver.com>
2023-11-23 17:59:45 +0900

    Feat: 로그인 화면

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-        <h3>메인홈입니다</h3>
-    </div>
+  <div>
+    <h3>메인홈입니다</h3>
+  </div>
+  <Login />
 </template>
 <script>
+import Login from "./Login.vue";
 export default {
-    
-}
+  components: { Login },
+};
 </script>
-<style>
-    
-</style>
+<style></style>
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,13 +1,31 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <h2>로그인</h2>
+    <form @submit.prevent="login">
+      <label for="username">사용자명:</label>
+      <input type="text" id="username" v-model="username" required />
+
+      <label for="password">비밀번호:</label>
+      <input type="password" id="password" v-model="password" required />
+
+      <button type="submit" @click="login">로그인</button>
+    </form>
+  </div>
 </template>
 <script>
 export default {
-    
-}
+  setup() {
+    const login = async () => {
+      try {
+        const res = await axios.post(`http://localhost:8880/login`);
+      } catch (error) {
+        console.log(err);
+      }
+    };
+    return {
+      login,
+    };
+  },
+};
 </script>
-<style>
-    
-</style>
+<style></style>
