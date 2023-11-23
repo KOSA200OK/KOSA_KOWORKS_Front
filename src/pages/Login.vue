3413c82618a723d41622825e8a70092a004be28d
aeokseung <oksh3012@naver.com>
2023-11-23 19:26:36 +0900

    Feat: login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -2,24 +2,31 @@
   <div>
     <h2>로그인</h2>
     <form @submit.prevent="login">
-      <label for="username">사용자명:</label>
-      <input type="text" id="username" v-model="username" required />
+      <label for="username">사원번호:</label>
+      <input type="text" id="memberId" v-model="memberId" required />
 
       <label for="password">비밀번호:</label>
       <input type="password" id="password" v-model="password" required />
 
-      <button type="submit" @click="login">로그인</button>
+      <button type="submit">로그인</button>
     </form>
   </div>
 </template>
 <script>
+import axios from "axios";
+import { ref } from "vue";
+
 export default {
   setup() {
+    const memberId = ref("");
+    const password = ref("");
     const login = async () => {
       try {
-        const res = await axios.post(`http://localhost:8880/login`);
+        const data = { memberId: memberId.value, password: password.value };
+        const res = await axios.post(`http://localhost:8880/login`, data);
+        console.log(res.data);
       } catch (error) {
-        console.log(err);
+        console.error(error);
       }
     };
     return {
