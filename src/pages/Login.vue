c31da9c76df7605408460e91e4cae73887d3eb2b
aeokseung <oksh3012@naver.com>
2023-11-30 11:34:20 +0900

    [Feat] storage에 저장해서 로그인 유지 기능 추가, logout기능 추가

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -10,6 +10,9 @@
 
       <button type="submit">Login</button>
     </form>
+    <form v-if="isLoggedIn" @submit.prevent="logout">
+      <button type="submit">Logout</button>
+    </form>
 
     <router-view v-if="isLoggedIn"></router-view>
   </div>
@@ -26,9 +29,10 @@ export default {
       isLoggedIn: false,
     };
   },
+  // component 생성되자마자 호출, 새로고침 할 때마다 호출
   created() {
     // 페이지가 로드될 때 세션 정보를 확인
-    this.checkSession();
+    this.findSession();
   },
   methods: {
     async login() {
@@ -38,19 +42,40 @@ export default {
           password: this.password,
         });
         this.isLoggedIn = true;
+        // 브라우저 스토리지에 로그인 상태 저장
+        localStorage.setItem("isLoggedIn", "true");
+
         this.$router.push("/");
         this.$data.user = response.data;
       } catch (error) {
-        console.error("Login failed:", error);
+        console.error("로그인 실패:", error);
       }
     },
 
-    async checkSession() {
+    async findSession() {
+      //----
+      // 브라우저 스토리지에서 로그인 상태 확인
+      const storedLoggedIn = localStorage.getItem("isLoggedIn");
+      this.isLoggedIn = storedLoggedIn === "true";
+
+      if (this.isLoggedIn) {
+        console.log("로그인 상태입니다. id: ");
+      } else {
+        console.log("로그인하지 않은 상태입니다.");
+      }
+    },
+
+    async logout() {
       try {
-        const response = await axios.get("http://localhost:8880/checkSession");
-        this.isLoggedIn = response.data;
+        await axios.get("http://localhost:8880/logout", {});
+        this.isLoggedIn = false;
+
+        // 브라우저 스토리지에서 로그인 상태 제거
+        localStorage.removeItem("isLoggedIn");
+
+        this.$router.push("/");
       } catch (error) {
-        console.error("Error checking session:", error);
+        console.error("로그아웃 실패:", error);
       }
     },
   },
