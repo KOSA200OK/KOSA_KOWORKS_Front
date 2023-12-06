21bf41726e4e83e803b7ea8d84429a2003b8ef46
오찬석 <dhcksehf1@naver.com>
2023-12-06 18:43:00 +0900

    [feat] pull받은 후 ... 이젠  무서워요..front2

--- src/router/index.js
+++ src/router/index.js
@@ -16,8 +16,9 @@ import RoomDetail from "@/pages/chat/RoomDetail.vue";
 import MeetingRoomList from "@/pages/meetingroom/MeetingRoomList.vue";
 import MeetingRoomResList from "@/pages/meetingroom/MeetingRoomResList.vue";
 import MeetingRoomTimeline from "@/pages/meetingroom/MeetingRoomTimeline.vue";
-import RoomList from "@/pages/chat/RoomList.vue";
-import RoomDetail from "@/pages/chat/RoomDetail.vue";
+
+
+
 
 import StuffReq from "@/pages/stuff/StuffReq.vue";
 const router = createRouter({
@@ -26,7 +27,7 @@ const router = createRouter({
     { path: "/home", component: Home },
     // { path: '/attendancelist', component: AttendanceList },
     { path: "/address/members", component: Address },
-    { path: "/chat/list", component: ChatRoomList },
+    // { path: "/chat/list", component: ChatRoomList },
     { path: "/chat/roomlist", component: RoomList },
     { path: "/chat/room/enter/:roomid", component: RoomDetail },
     // { path: '/calendar/list', component: Attendance },
