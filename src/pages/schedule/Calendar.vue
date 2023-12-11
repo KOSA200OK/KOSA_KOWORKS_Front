f46048eef66a6ba1ea267032bde53d6f58131805
nawonhee <nwh0921@naver.com>
2023-12-11 21:10:48 +0900

    [Feat] 일정관리 - 일정 조회 구현중

--- src/pages/schedule/Calendar.vue
+++ src/pages/schedule/Calendar.vue
@@ -1,6 +1,10 @@
 <template>
 <main>
-    <FullCalendar :options="calendarOptions" />
+    <FullCalendar ref="fullCalendar" :options="calendarOptions">
+        <template v-slot:eventContent='arg'>
+            <b>{{ arg.event.title }}</b>
+        </template>
+    </FullCalendar>
 </main>
 </template>
 <script>
@@ -16,10 +20,48 @@ export default {
     data() {
         return {
             calendarOptions: {
-            plugins: [ dayGridPlugin, interactionPlugin ],
-            initialView: 'dayGridMonth'
+                plugins: [ dayGridPlugin, interactionPlugin ],
+                initialView: 'dayGridMonth',
+                dateClick: this.handleDateClick,
+                events: null
+            }
         }
+    },
+    methods: {
+        getCurrentMonth() {
+            // FullCalendar의 date 객체를 사용하여 현재 월을 얻기
+            const currentMonth = this.$refs.fullCalendar.getApi().getDate().format('YYYY-MM');
+
+            // 콘솔에 출력 또는 원하는 로직 수행
+            console.log('Current Month:', currentMonth);
+        },
+        scheduleHandler(){
+            const memberId = localStorage.getItem('memberId')
+            console.log(memberId)
+            const url = `${this.backURL}/schedule/calendar?memberId=${memberId}&month=12`
+            axios.get(url,{params : this.data})
+            .then(response=>{
+                console.log(response.data)
+                if(response.data){
+                    for(const data of response.data){
+                        this.calendarOptions.events[i].title = data.title
+                        this.calendarOptions.events[i].start = data.startTime
+                        this.calendarOptions.events[i].end = data.endTime
+                    }
+                }
+            })
+            .catch((Error)=>{
+                console.log(Error)
+            })
+        },
+         handleDateClick: function(arg) {
+            alert('date click! ' + arg.dateStr)
+        }
+    },
+    mounted(){
+        this.scheduleHandler()
+        let calendarApi = this.$refs.fullCalendar.getApi()
+        console.log(calendarApi)
     }
-  }
 }
 </script>
