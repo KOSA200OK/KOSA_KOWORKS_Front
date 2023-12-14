b981a59ce9be3a91ff213c45d5e9af820dea717d
Logan-CatKeeper <142392396+Logan-CatKeeper@users.noreply.github.com>
2023-12-14 14:38:55 +0900

    [STYLE] 메인페이지 레이아웃 및 스타일 수정

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,22 +3,43 @@
 		<div class="gridContainer">
 			<div class="item attendance"> <!-- 찬석 -->
 				<h3>근태 현황</h3>
-				<br>
-				<h3>{{ currentTime }}</h3>
-				<br>
-				<h3 v-bind="attendanceTime">출근 시간: {{ attendanceTime }}</h3>
-				<h3 v-bind="offTime">퇴근 시간: {{ offTime }}</h3>
-				<button @click="attendanceHandler">출근</button> 
-				<button @click="attendance2Handler">퇴근</button>
+				<div class="box">
+					<div class="currentDay">{{ currentDay }}</div>
+					<h3>{{ currentTime }}</h3>
+					<br>
+					<h3>출근 시간: {{ attendanceTime }}</h3>
+					<h3>퇴근 시간: {{ offTime }}</h3>
+					<button @click="attendanceHandler">출근</button>
+					<button @click="attendance2Handler">퇴근</button>
+				</div>
+			</div>
+			<div class="item stuff">
+				<h3>비품 신청현황</h3>
+				<div class="box">
+
+				</div>
 			</div>
 			<div class="item notice">
 				<h3>최근 공지사항</h3>
-				<button class="add-button" @click="addNotice">
-					<span class="material-icons">add</span>
-				</button>
-				<hr>
+				<div class="box">
+					<button class="add-button" @click="addNotice">
+						<span class="material-icons">add</span>
+					</button>
+					<hr>
+				</div>
 			</div>
-			<div class="item calendar">C</div>
+			<div class="item calendar">
+				<h3>일정</h3>
+				<div class="box">
+
+				</div>
+			</div>
+			<div class="item notification">
+				<h3>알림</h3>
+				<div class="box">
+
+				</div>
+			</div>
 		</div>
 	</main>
 </template>
@@ -29,29 +50,35 @@ export default {
 	components: { Sidebar },
 	data() {
 		return {
+			currentDay: this.getCurrentDay(),
 			currentTime: this.getCurrentTime(),
-			attendanceTime: null, 
+			attendanceTime: null,
 			offTime: null,
 
 		};
 	},
 	mounted() {
-			setInterval(() => {
-				this.currentTime = this.getCurrentTime();
-			}, 1000);
-		},
+		setInterval(() => {
+			this.currentTime = this.getCurrentTime();
+		}, 1000);
+	},
 	methods: {
-		getCurrentTime() {
+		getCurrentDay() {
 			const now = new Date();
 			const year = now.getFullYear();
 			const month = (now.getMonth() + 1).toString().padStart(2, '0');
 			const day = now.getDate().toString().padStart(2, '0');
 			const days = ['일', '월', '화', '수', '목', '금', '토'];
 			const dayOfWeek = days[now.getDay()];
+			return `${year}년 ${month}월 ${day}일 (${dayOfWeek})`;
+		},
+
+		getCurrentTime() {
+			const now = new Date();
 			const hours = now.getHours().toString().padStart(2, '0');
 			const minutes = now.getMinutes().toString().padStart(2, '0');
 			const seconds = now.getSeconds().toString().padStart(2, '0');
-			return `${year}년 ${month}월 ${day}일 (${dayOfWeek}) ${hours}시 ${minutes}분 ${seconds}초`;
+			return `${hours}시 ${minutes}분 ${seconds}초`;
 		},
 
 
@@ -113,83 +140,38 @@ export default {
 <style>
 .gridContainer {
 	display: grid;
-	grid-template-columns: 3fr 5fr;
+	grid-template-columns: 1fr 1fr 1fr 1fr;
+	grid-template-rows: repeat(2, 400px);
 	/* grid-template-rows: repeat(2, minmax(400px, auto)); */
-	grid-template-rows: 250px 550px;
-	row-gap: 30px;
+	/* grid-template-rows: 250px 550px; */
+	row-gap: 40px;
 	column-gap: 20px;
 
 }
 
-.item {
-	border-radius: 30px;
-	background-color: ghostwhite;
-	box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-	/* 그림자 효과 추가 */
+.attendance{
+	grid-column-start: 1;
 }
 
-.attendance {
-	text-align: center;
-	padding: 20px;
-
+.notification{
+	grid-row-start: 2;
+	grid-column-end: span 2;
 }
 
 .notice{
-	position: relative;
+	grid-column: 3 / 5;
+	grid-row-start: 2;
 }
 
 .calendar{
-	grid-column-start: 2;
-	grid-row : 1 / 3;
+	grid-column-start: 4;
 }
 
-.notice span{
-	color: black;
-}
-
-.notice h3{
-	text-align: center;
-	padding: 20px;
-}
-
-.item button {
-  display: inline-block;
-  padding: 10px 20px;
-  margin-top: 15px;
-  border: 2px solid #3498db; 
-  border-radius: 5px;
-  background-color: transparent; 
-  color: #3498db; 
-  font-size: 16px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-}
-
-.item button:hover {
-  background-color: #3498db; 
-  color: #fff; 
-}
-
-.item .add-button {
-	display: inline-block;
-	position: absolute;
-	top: 0;
-	right: 0; 
-	padding: 10px;
-	border: none;
-	border-radius: 50%;
-	background-color: transparent;
-	color: #fff;
-	font-size: 18px;
-	cursor: pointer;
-	transition: background-color 0.3s ease;
-}
-
-.item .add-button:hover {
-	background-color: #2980b9;
-}
-
-.item button + button {
-  margin-left: 10px;
+.box {
+  height: 350px;
+  border: 1px solid #ccc;
+  border-radius: 15px;
+  box-shadow: 10px 10px 4px rgba(0, 0, 0, 0.1);
+  padding: 20px;
 }
 </style>
